7a456e26abfb24492dd3e86e76c151b9cab17df9
IamDaGod-kira <IamDaGod_MoonDeity@protonmail.com>
2025-10-12 14:53:57 +0530

    added workflow

--- webapp-firebase/src/App.jsx
+++ webapp-firebase/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div>
-        <h1 className="text-blue-950">hello</h1>
+        
       </div>
     </>
   );
---
+++ webapp-firebase/src/components/subParts/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header () {
+    return (
+        <>
+            <header>
+                <h3 className="">Smart Study Schedule Generator</h3>
+            </header>
+        </>
+    )
+}
