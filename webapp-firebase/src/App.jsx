09451cfef98ab8db78a229f852c7a7c3c9b30374
IamDaGod-kira <IamDaGod_MoonDeity@protonmail.com>
2025-10-14 15:30:36 +0530

    added header..

--- webapp-firebase/src/App.jsx
+++ webapp-firebase/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Header from "./components/subParts/header";
 
 function App() {
   return (
     <>
       <div>
-        
+        <Header />
       </div>
     </>
   );
