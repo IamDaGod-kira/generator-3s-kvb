800e84d94f7df78a030d2c545224b75ca5b00691
IamDaGod-kira <IamDaGod_MoonDeity@protonmail.com>
2025-10-19 18:31:16 +0530

    test2

--- webapp-firebase/vite.config.js
+++ webapp-firebase/vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+const basePath = process.env.VITE_BASE_PATH || "/";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: basePath,
 });
